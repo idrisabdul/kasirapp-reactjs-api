2527ddb3f574db22034278dcbfa6d6cbcacb572b
idrisabdul <idrisaziz52@gmail.com>
2021-07-28 07:44:46 +0700

    react with fake api

---
+++ src/component/Menus.js
@@ -0,0 +1,19 @@
+import { Col, Card, } from 'react-bootstrap'
+
+const Menus = ({ menu }) => {
+    return (
+        <Col md={4} xs={6} className="mb-4">
+            <Card >
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{menu.nama}</Card.Title>
+                    <Card.Text>
+                        Rp. {menu.harga}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
+
+export default Menus
